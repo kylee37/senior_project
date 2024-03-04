29fd9e422190bbaf8cc9b7074275a10c76a8c989
kylee37 <kylee2998@gmail.com>
2024-03-04 20:00:04 +0900

    Fundamental

---
+++ Assets/ButtonEvent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextDay()
+    {
+        Debug.Log("다음 운영일로");
+        // 정산 후 다음 운영일로 넘어간다.
+    }
+}
